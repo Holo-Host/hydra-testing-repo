08a350c162cf38e311571758ea2575c581c6ba5e
Marek Mahut <marek.mahut@gmail.com>
2020-05-18 11:01:41 +0200

    Create nixpkgs.nix

---
+++ nixpkgs.nix
@@ -0,0 +1,4 @@
+import (fetchTarball {
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/68bc1d2b1bced406d00648f11e9bb75b2e99050d.tar.gz";
+  sha256 = "0brv4n0bmv4g6midlx701r0c75m2y97dlsdqffp77lk7m2ml7zwb";
+})
